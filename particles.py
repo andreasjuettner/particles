6fbfe0107b7f02dd220330f85b257d8fb43026a3
juettner <juettner@soton.ac.uk>
2024-03-13 17:53:25 +0100

    Added extended info facility

--- particles.py
+++ particles.py
@@ -16,13 +16,107 @@
 
 import numpy as np
 import h5py
+def depth(d):
+     if isinstance(d,dict):
+      # https://stackoverflow.com/questions/23499017/know-the-depth-of-a-dictionary
+      return 1 + (max(map(depth, d.values())) if d else 0)
+#     elif isinstance(d,list):
+#      return 1 + (max(map(depth, d))) if d else 0 
+     else:
+      return 0
+def read_hdf5_file(file,d):
+    for key,val in file.items():
+        if type(val) == h5py._hl.dataset.Dataset:
+            d[key] = np.array(val,d[key])
+#             print(key,np.array(val))
+
+        else:
+            d[key] = read_hdf5_file(val)
+    return d
+
+def info_to_dict(h5in,out):
+    for key,item in h5in.items():
+     if isinstance(item,h5py.Dataset):
+       #print(key,item[()],np.array(item))
+       if h5in[key]:
+        try:
+         out[key] = item[()]
+        except:
+         
+         print('cannot read ',key,item)
+     elif isinstance(item,h5py.Group):
+      tmp = {}
+      out[key] = info_to_dict(item,{})
+     else:
+      print('info_to_dict: noideia')
+      exit()
+    return out
+
+def dict_to_info(group,dictionary):
+    d = depth(dictionary)
+    if d == 0:
+     print('dict_to_info: not a dictionary')
+     exit()
+    if d == 1:
+     for key,value in dictionary.items():
+        group[key] = value 
+     return group
+    else:
+     for key, value in dictionary.items():
+        if isinstance(value, dict):
+            # Recursively decompose nested dictionaries
+            subgroup = group.create_group(key)
+            subgroup = dict_to_info(subgroup,value)
+        else:
+            # Handle non-dictionary values
+            try:
+              if isinstance(value,bytes):
+               group[key] = value.decode()
+              else:
+               group[key] = value
+            except:
+               continue
+    return group
+
+def fmean(bs,bare_data):
+    return np.mean(bare_data[bs,:],0)
+try:
+    import multiprocessing
+    MULTIPROCESSING = True
+except ImportError:
+    print('Module multiprocessing not found -- proceed with out it')
+    print('Consider installing the module for better performance')
+    MULTIPROCESSING = False
+    pass
+def par_kernel(chunk):
+    f           = chunk[1]
+    args        = chunk[2]
+    res         = np.apply_along_axis(f,1,chunk[0],*args)
+    return res
+# apply_along_axis for multprocessing -- not supported, under development
+def my_apply_along_axis(f,samples,*args,Nproc=6,parallel=True):
+    if (MULTIPROCESSING == False) or (parallel==False):
+     # use single-core version
+     return np.apply_along_axis(f,1,samples,*args)
+    else:
+     # parallelise over Nproc cores
+     # Chunks for the mapping (only a few chunks):
+     chunks     = [(sub_arr,f,args) for sub_arr in np.array_split(samples, Nproc)]
+     pool       = multiprocessing.get_context("fork").Pool(Nproc)
+     individual_results = pool.map(par_kernel, chunks)
+     # Freeing the workers:
+     pool.close()
+     pool.join()
+     return np.concatenate(individual_results)
+
+
 class particle():
   """
    PARTICLES v0
   """
   def __new__(cls, *args, **kwargs):
     return super().__new__(cls)
-  def __init__(self,seed=1,Nboot=1000,bare_data=[]):
+  def __init__(self,seed=1,Nboot=1000,bare_data=[],parallel=False,Nproc=1):
    """
     initiatlisation
    """
@@ -36,6 +130,9 @@ class particle():
    self.N_bs   	= Nboot
    self.bs_seed = seed
    self.bare_data= bare_data
+   self.parallel = parallel
+   self.Nproc	 = Nproc
+   self.NEWINFO  = True # improved version for storing info
    # if class initalised with list or array, create samples
    if isinstance(bare_data, list):
     if bare_data:
@@ -53,6 +150,12 @@ class particle():
     compute bootstrap error
    """
    return np.sqrt(np.diag(self.covariance()))
+  def printnice(self):
+   try: 
+    import jn
+    return jn.disperr(self.bs_val(),self.bs_dval())
+   except:
+    return('')
   def covariance(self):
     """ 
      compute covariance matrix
@@ -83,8 +186,12 @@ class particle():
     self.bs_indices = np.random.randint(0,high=N,size=(N,K)).T
    # I checked -- apply_along_axis takes about the same time as list comprehension 
    # (commented out below)
-   fmean = lambda bs: np.mean(self.bare_data[bs,:],0)
-   self.samples = np.apply_along_axis(fmean,1,self.bs_indices)
+   #fmean2 = lambda bs: np.mean(self.bare_data[bs,:],0)
+   #self.samples = np.apply_along_axis(fmean2,1,self.bs_indices)
+   #print(self.samples)
+   self.samples = my_apply_along_axis(fmean,self.bs_indices,self.bare_data,Nproc=self.Nproc,parallel=self.parallel)
+   #print(self.samples)
+   #exit()
 #   tmp = np.array([np.mean(self.bare_data[bs,:],0) for bs in self.bs_indices])
    # clean up results after new data loaded
    self.cov = np.array([])
@@ -105,16 +212,25 @@ class particle():
     if self.bare_data!=[]:
 #     print "storing bare_data"
      f.create_dataset('bare_data',data=self.bare_data)
-    for item in self.info.keys():
-     if isinstance(self.info[item],list):
-      if any(isinstance(i, list) for i in self.info[item]): # check if nested list
-       for ii,l in enumerate(self.info[item]):
-        f.create_dataset('info_'+item+'_'+str(ii),data=l)
+    #
+    if self.NEWINFO:
+     #
+     # Create a group to store the dictionary
+     group = f.create_group('info')
+     group = dict_to_info(group,self.info)
+     #
+    else: # old version
+     for item in self.info.keys():
+      if isinstance(self.info[item],list):
+       if any(isinstance(i, list) for i in self.info[item]): # check if nested list
+        for ii,l in enumerate(self.info[item]):
+         f.create_dataset('info_'+item+'_'+str(ii),data=l)
+       else:
+         #print(item,self.info[item])
+         f.create_dataset('info_'+item+'_'+str(0),data=self.info[item])
       else:
-        #print(item,self.info[item])
-        f.create_dataset('info_'+item+'_'+str(0),data=self.info[item])
-     else:
-      f.create_dataset('info_'+item,data=self.info[item])
+       f.create_dataset('info_'+item,data=self.info[item])
+     
     f.close
   def read(self,f):
     """
@@ -126,8 +242,25 @@ class particle():
     self.bs_mean = np.array(f.get('bs_mean'))
     if 'bare_data' in f:
      self.bare_data = np.array(f.get('bare_data'))
-      
     info={}
+    for item in f.keys():
+     if 'info' in item:
+      dat = np.array(f.get(item))
+      info[item.replace('info_','')]=dat
+    self.info = info
+    f.close
+  def read2(self,f):
+    """
+     read content from disk
+    """
+    f=h5py.File(f,'r')
+    self.samples = np.array(f.get('samples'))
+    self.central = np.array(f.get('central'))
+    self.bs_mean = np.array(f.get('bs_mean'))
+    if 'bare_data' in f:
+     self.bare_data = np.array(f.get('bare_data'))
+    info={}
+    info = info_to_dict(f.get('info'),info)
     for item in f.keys():
      if 'info' in item:
       dat = np.array(f.get(item))
